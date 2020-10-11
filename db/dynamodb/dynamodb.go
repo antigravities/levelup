8724582a57505dc4c5a38d5bebc2a77e9c402110
Alexandra Frock <im@alexandra.moe>
2020-10-10 20:06:55 -0400

    Also project LastUpdate, RecommendedAt, Review in FindStaleApps

--- db/dynamodb/dynamodb.go
+++ db/dynamodb/dynamodb.go
@@ -204,7 +204,7 @@ func FindStaleApps() []types.App {
 
 	res, err := db.Scan(&dynamodb.ScanInput{
 		TableName:            aws.String(table),
-		ProjectionExpression: aws.String("AppID, LastUpdate"),
+		ProjectionExpression: aws.String("AppID, LastUpdate, RecommendedAt, Review"),
 		FilterExpression:     aws.String("LastUpdate < :t AND IsPending <> :f"),
 		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
 			":t": {
